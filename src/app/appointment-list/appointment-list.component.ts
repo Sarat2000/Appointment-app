34988787eb554b481f4e75aae0cbac8953a4ea9f
Sarat2000 <gsaratchandra27@gmail.com>
2023-12-21 05:35:52 +0530

    Basic Angular template

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css'
+})
+export class AppointmentListComponent {
+
+  appointment: Appointment = {
+    id: '1',
+    title: 'Take dog for a walk',
+    date: new Date('2023-07-01')
+  }
+
+}
